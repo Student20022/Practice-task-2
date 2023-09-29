f018375518f0dad91d8700ac294abf66f5f84b38
Student20022 <131999657+Student20022@users.noreply.github.com>
2023-09-29 19:06:36 +0300

    Add files via upload

---
+++ 1/1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть рядок: ");
+        string inputString = Console.ReadLine();
+
+        Console.Write("Введіть букву: ");
+        char letter = Console.ReadKey().KeyChar;
+
+        int firstIndex = inputString.IndexOf(letter);
+        int lastIndex = inputString.LastIndexOf(letter);
+
+        if (firstIndex != -1 && lastIndex != -1)
+        {
+            string substring = inputString.Substring(firstIndex + 1, lastIndex - firstIndex - 1);
+            int count = substring.Count(c => c == letter);
+            Console.WriteLine($"\nКількість входжень букви між першим і останнім: {count}");
+        }
+        else
+        {
+            Console.WriteLine("\nБуква не знайдена в рядку.");
+        }
+    }
+}
